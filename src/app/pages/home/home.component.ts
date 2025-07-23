0695fb106ea0564ee32b678280ab4d013f1c7811
Jameslurtin <jameslurtin@gmail.com>
2025-07-23 12:09:58 +0200

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,30 @@
-import {Component, inject} from '@angular/core';
-import {UtilityService} from '../../utils/utility.service';
-import {RouterModule} from '@angular/router';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ButtonModule} from 'primeng/button';
+import { Component, inject } from '@angular/core';
+import { UtilityService } from '../../utils/utility.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports:  [CommonModule, ReactiveFormsModule, FormsModule, RouterModule, ButtonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    ButtonModule,
+    DropdownModule,
+    InputTextModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  utilityService = inject(UtilityService);
 
- utilityService=inject(UtilityService);
   login() {
     this.utilityService.navigate('/login');
   }
@@ -22,4 +32,13 @@ export class HomeComponent {
   register() {
     this.utilityService.navigate('/register');
   }
+  locations = [
+  { label: 'Remote', value: 'Remote' },
+  { label: 'Nearby', value: 'Nearby' },
+  { label: 'New York', value: 'NY' },
+  { label: 'California', value: 'CA' }
+];
+
+
+
 }
